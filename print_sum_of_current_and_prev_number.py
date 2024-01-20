9668c506d86a0569d0c5749964c9e8915256ee74
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-20 23:15:49 +0800

    Final

--- print_sum_of_current_and_prev_number.py
+++ print_sum_of_current_and_prev_number.py
@@ -22,6 +22,8 @@
 #   print(current which is i, previous num, sum)
 #   update the previous number
 
+print("Printing current and previous number sum in a range(10)")
+
 previous_num = 0
 for current in range(10):
     print(f"Current number: {current}. Previous number: {previous_num}. Sum: {current + previous_num}")
